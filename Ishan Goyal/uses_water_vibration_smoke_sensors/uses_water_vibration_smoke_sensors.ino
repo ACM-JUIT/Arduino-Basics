b192195c64fb75b167b5bec078fac380a47e3dd5
ishangoy <35898293+ishangoy@users.noreply.github.com>
2018-02-14 20:24:07 +0530

    Add files via upload

---
+++ Ishan Goyal/uses_water_vibration_smoke_sensors/uses_water_vibration_smoke_sensors.ino
@@ -0,0 +1,25 @@
+int a=A0,b=A1,c=8,d,e,f,h;
+void setup()
+{
+  pinMode(a,INPUT);
+  pinMode(b,INPUT);
+  pinMode(c,INPUT);
+Serial.begin(9600);
+}
+
+  void loop()
+  {
+    Serial.print(d); 
+    Serial.println(" d is water sensor");
+    d=analogRead(a);
+    Serial.print(e);
+    Serial.println("e is smoke sensor");
+    e=analogRead(b);
+    Serial.print(f);
+    Serial.println("f is vibration sensor");
+    f=digitalRead(c);  
+  
+
+  }
+    
+
